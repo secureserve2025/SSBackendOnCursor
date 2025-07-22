9323404e63aef8e9276a569e46089ad0fabe6a40
umaavadh <uma.avadh@gmail.com>
2025-07-22 16:33:03 -0400

    Add Login dropdown with Freelancer and Client options

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
-import FreelancerLogin from './components/FreelancerLogin.tsx';
-import ClientLogin from './components/ClientLogin.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login/freelancer" element={<FreelancerLogin />} />
-        <Route path="/login/client" element={<ClientLogin />} />
-      </Routes>
-    </Router>
+    <App />
   </StrictMode>
 );
