76d94042e49406edaa8b2b538538d0b34f8bbe6d
umaavadh <uma.avadh@gmail.com>
2025-07-23 17:03:14 -0400

    Increase font size of freelancer login description

--- src/pages/FreelancerLogin.tsx
+++ src/pages/FreelancerLogin.tsx
@@ -55,7 +55,7 @@ const FreelancerLogin: React.FC = () => {
                 <User className="h-8 w-8 text-white" />
               </div>
             </div>
-            <p className="text-cyan-100 text-sm">
+            <p className="text-cyan-100 text-base">
               Access your freelance dashboard and manage your projects
             </p>
           </div>
